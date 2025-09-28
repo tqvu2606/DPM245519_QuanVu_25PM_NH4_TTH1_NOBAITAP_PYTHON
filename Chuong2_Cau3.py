0e1af8fcf2233e9de3f307416a2366142014862f
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-09-28 23:08:24 +0700

    Create Chuong2_Cau3.py

---
+++ Chuong2_Cau3.py
@@ -0,0 +1,7 @@
+# Câu 3: Tính điểm trung bình
+toan=float(input("Nhập điểm Toán:"))
+ly=float(input("Nhập điểm lý:"))
+hoa=float(input("Nhập điểm hóa:"))
+dtb=(toan+ly+hoa)/3
+print("Điểm trung bình=",dtb)
+print("Điểm trung bình=",round(dtb,2))
