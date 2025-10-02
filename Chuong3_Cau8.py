b77bb6b690bb8fe12540bd62e0f696ed986fea2a
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-02 08:15:15 +0700

    Create Chuong3_Cau8.py

---
+++ Chuong3_Cau8.py
@@ -0,0 +1,21 @@
+a = int(input("nhập vào giá trị a:"))
+
+b = int(input("nhập vào giá trị b:"))
+
+
+
+i=a+b
+
+print("a+b: ",i)
+
+j=a-b
+
+print("a-b: ",j)
+
+k=a*b
+
+print("a*b: ",k)
+
+l=a/b
+
+print("a/b: ",l)
