db5fea902656c91ba18f76b0ed09630265a790c8
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-02 08:03:19 +0700

    Create Chuong2_Cau10.py

---
+++ Chuong2_Cau10.py
@@ -0,0 +1,21 @@
+#Câu 10: Hãy viết ngắn gọn lại các lệnh dưới đây
+# (a)
+x += 1
+
+# (b)
+x /= 2
+
+# (c)
+x = -x   # (trường hợp này không có toán tử rút gọn phù hợp)
+
+# (d)
+x += y
+
+# (e)
+x -= (y + 7)
+
+# (f)
+x *= 2
+
+# (g)
+number_of_closed_cases += 2 * ncc
