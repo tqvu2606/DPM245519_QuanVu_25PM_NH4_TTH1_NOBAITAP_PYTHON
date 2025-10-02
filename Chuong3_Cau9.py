85cc0844a27d0840cbb2a4f9b47b0fea1baa219c
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-02 08:15:57 +0700

    Create Chuong3_Cau9.py

---
+++ Chuong3_Cau9.py
@@ -0,0 +1,21 @@
+def tim_quy_trong_nam(Thang):
+
+    quy = (Thang-1)//3+1
+
+    return quy
+
+
+
+thang = int(input("Xin vui lòng nhập vào 1 tháng:"))
+
+
+
+if(1<= thang <= 12):
+
+    quy = tim_quy_trong_nam(thang)
+
+    print(f"Tháng {thang} thuộc vào quý {quy} trong năm.")
+
+else:
+
+    print("Tháng không hợp lệ!!!(SOS), xin vui lòng nhập lại :)))")
