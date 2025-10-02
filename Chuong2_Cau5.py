d36f0659844fbca072e3b1345b38d0cfedb6352a
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-02 07:58:02 +0700

    Create Chuong2_Cau5.py

---
+++ Chuong2_Cau5.py
@@ -0,0 +1,14 @@
+#Trình bày các loại ghi chú trong Python.
+
+Trong Python, có hai loại ghi chú (comment):
+
+Ghi chú một dòng: Dùng ký tự # ở đầu dòng. Ví dụ:
+# Đây là ghi chú một dòng
+x = 5  # Ghi chú sau lệnh
+Ghi chú nhiều dòng: Dùng ba dấu nháy đơn ''' hoặc ba dấu nháy kép """ Ví dụ:
+'''
+Đây là ghi chú
+nhiều dòng
+'''
+print("Hello")
+ c5
