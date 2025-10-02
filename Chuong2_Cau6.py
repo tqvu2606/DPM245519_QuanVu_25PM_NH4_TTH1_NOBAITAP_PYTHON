de17aa7886b3efd12a2774854792281a54fef60a
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-02 07:59:14 +0700

    Create Chuong2_Cau6.py

---
+++ Chuong2_Cau6.py
@@ -0,0 +1,11 @@
+/: Chia lấy kết quả là số thực. Ví dụ: 5 / 2 = 2.5
+//: Chia lấy phần nguyên. Ví dụ: 5 // 2 = 2
+%: Chia lấy phần dư (modulo). Ví dụ: 5 % 2 = 1
+**: Lũy thừa. Ví dụ: 2 ** 3 = 8
+and: Toán tử logic "và", trả về True nếu cả hai biểu thức đều đúng.
+or: Toán tử logic "hoặc", trả về True nếu ít nhất một biểu thức đúng.
+is: So sánh hai đối tượng có cùng vị trí trong bộ nhớ hay không. Trả về True nếu đúng.
+is not : So sánh hai đối tượng không cùng vị trí trong bộ nhớ hay không .Trả về True nếu đúng.
+in :Kiểm tra một giá trị có tồn tại trong một chuỗi hoặc danh sách hay không . Trả về True nếu đúng.
+not in: Kiểm tra một giá trị không tồn tại trong một chuỗi hoặc danh sách hay không . Trả về True nếu đúng.
+not: Phủ định một biểu thức Logic . Trả về True nếu giá trị là False.
