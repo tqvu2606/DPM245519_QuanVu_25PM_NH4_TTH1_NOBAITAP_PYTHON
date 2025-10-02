4ae97d6e2c615bcbb8890cdbd1232a0c5f4d763c
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-02 07:56:59 +0700

    Create Chuong2_Cau4.py

---
+++ Chuong2_Cau4.py
@@ -0,0 +1,19 @@
+Python hỗ trợ các kiểu dữ liệu cơ bản sau:
+
+int (số nguyên)
+float (số thực)
+bool (kiểu logic: True/False)
+str (chuỗi ký tự)
+list (danh sách)
+tuple (bộ giá trị)
+set (tập hợp)
+dict (từ điển)Python hỗ trợ các kiểu dữ liệu cơ bản sau:
+
+int (số nguyên)
+float (số thực)
+bool (kiểu logic: True/False)
+str (chuỗi ký tự)
+list (danh sách)
+tuple (bộ giá trị)
+set (tập hợp)
+dict (từ điển)
