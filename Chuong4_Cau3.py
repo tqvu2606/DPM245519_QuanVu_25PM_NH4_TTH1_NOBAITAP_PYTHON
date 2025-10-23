8810fd414b07895638d629547e4d9f5dfde29409
tqvu2606 <vu_dpm245519@student.agu.edu.vn>
2025-10-23 08:21:36 +0700

    Chuong4_Cau3.py

--- Chuong4_Cau3.py
+++ Chuong4_Cau3.py
@@ -17,3 +17,20 @@ def NguyCoBenh(bmi):
  if bmi<18.5:
  return "Thấp"
  elif bmi<=24.9
+return "Trung Bình"
+ elif bmi<=29.9:
+ return "Cao"
+ elif bmi<=34.9:
+ return "Cao"
+ elif bmi<=39.9:
+ return "Rất cao"
+ else:
+ return "Nguy Hiểm"
+print("Nhập vào chiều cao:")
+height=float(input())
+print("Nhập vào cân nặng:")
+weight=float(input())
+bmi=BMI(height,weight)
+print("BMI của bạn=",bmi)
+print("Phân loại bạn=",PhanLoai(bmi))
+print("Nguy cơ bệnh của Thím=",NguyCoBenh(bmi))
